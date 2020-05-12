e7a74eb55cd3e8fba12ff08e1932980bcbb74703
matt joseloff <msjo425@gmail.com>
2020-05-12 20:47:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function iterativeLog(array){
+array.forEach(printOut)
+}
+
+function printOut(item, index, array){
+  console.log(`${index}: ${item}`)
+}
+
+function iterate(outsider){
+  list = ["ben","nick"]
+  list.forEach(outsider)
+  return list
+}
+
+function doToArray(array,callback){
+  array.forEach(callback)
+}
